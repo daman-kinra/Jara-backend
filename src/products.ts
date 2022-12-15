ea9ee12e4ff0201d0c00ebe73bd62a08335912c2
Damandeep Kinra <daman-kinra@Damandeeps-MacBook-Air.local>
2022-12-16 00:52:40 +0530

    with error and working route

--- src/products.ts
+++ src/products.ts
@@ -1,9 +1,13 @@
+import { resolveSoa } from "dns";
 import express from "express";
 const router = express.Router();
 const obj = {};
 router.get("/home", (req, res) => {
   console.log(obj.name.surname);
   res.send("Hello from home yess");
+});
+router.get("/", (req, res) => {
+  res.send("Yooo");
 });
 
 export default router;
